d0bf42d44c8a7bc3e95398ff99342686c517baee
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2026-03-26 17:25:29 -0700

    Declare enum GameId and a sealed record GameInstall

--- src/SADeckManager.Core/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SADeckManager.Core;
-
-public class Class1
-{
-
-}
---
+++ src/SADeckManager.Core/GameInstall.cs
@@ -0,0 +1,15 @@
+﻿namespace SADeckManager.Core;
+
+public enum GameId
+{
+    SonicAdventureDX, // SA1
+    SonicAdventure2 // SA2
+}
+
+public sealed record GameInstall(
+    GameId Id,
+    string SteamAppId,
+    string LibraryRoot,
+    string InstallDir,
+    string ProtonPrefixDir
+);
