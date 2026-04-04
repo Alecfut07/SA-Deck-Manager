d54ae28f9b5097d9a4c6dd06424f30a0fed6a57e
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2026-04-03 17:06:33 -0700

    Program.cs - prevent reuse mutex names from another mod manager

--- src/SADeckManager/Program.cs
+++ src/SADeckManager/Program.cs
@@ -31,7 +31,6 @@ class Program
         }
     }
 
-    // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
