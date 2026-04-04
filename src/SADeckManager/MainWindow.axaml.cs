25ddcc8e499b82a2055dd7286955a03c5621094a
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2026-04-03 17:07:22 -0700

    MainWindow.axaml.cs - Start the server on the primary instance

--- src/SADeckManager/MainWindow.axaml.cs
+++ src/SADeckManager/MainWindow.axaml.cs
@@ -24,6 +24,7 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        SingleInstanceIpc.StartFocusServer(this);
         LoadData();
     }
 
